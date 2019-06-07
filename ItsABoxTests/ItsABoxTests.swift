7e6b636fd5ce699d6728d7e2f3376bbee1afbfe4
Bratislav Ljubisic <bratislav.ljubisic@gmail.com>
2019-06-07 21:27:59 +0200

    Initial Commit

---
+++ ItsABoxTests/ItsABoxTests.swift
@@ -0,0 +1,34 @@
+//
+//  ItsABoxTests.swift
+//  ItsABoxTests
+//
+//  Created by Bratislav Ljubisic on 07.06.19.
+//  Copyright © 2019 Bratislav Ljubisic. All rights reserved.
+//
+
+import XCTest
+@testable import ItsABox
+
+class ItsABoxTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
