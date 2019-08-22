58a91d8a0d9f1cef3d85834dac291a71e5a178d6
Bratislav Ljubisic <bratislav.ljubisic@gmail.com>
2019-08-22 15:25:48 +0200

    Added tap gesture so now a box can be added to the node.

--- ItsABox/View/ViewController.swift
+++ ItsABox/View/ViewController.swift
@@ -22,6 +22,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneManager.attach(to: sceneView)
         sceneManager.displayDebugInfo()
         
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTapScene(_:)))
+        view.addGestureRecognizer(tapGesture)
+        
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -29,5 +32,49 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+    }
+    
+    @objc func didTapScene(_ gesture: UITapGestureRecognizer) {
+        switch gesture.state {
+        case .ended:
+            //1
+            let location = gesture.location(ofTouch: 0,
+                                            in: sceneView)
+
+            //2
+            let hit = sceneView.hitTest(location,
+                                        types: .existingPlaneUsingGeometry)
+            //3
+            if let hit = hit.first {
+                placeBlockOnPlaneAt(hit)
+            }
+        default:
+            break
+        }
+    }
+    
+    func placeBlockOnPlaneAt(_ hit: ARHitTestResult) {
+       //1
+        let box = createBox()
+        position(node: box, atHit: hit)
+        //2
+        sceneView?.scene.rootNode.addChildNode(box)
+    }
+    
+    private func createBox() -> SCNNode {
+        let box = SCNBox(width: 0.15, height: 0.20, length: 0.02, chamferRadius: 0.02)
+        let boxNode = SCNNode(geometry: box)
+
+        return boxNode
+    }
+    
+    private func position(node: SCNNode, atHit hit: ARHitTestResult) {
+        //1
+        node.transform = SCNMatrix4(hit.anchor!.transform)
+        //2
+        node.eulerAngles = SCNVector3Make(node.eulerAngles.x + (Float.pi / 2), node.eulerAngles.y, node.eulerAngles.z)
+        //3
+        let position = SCNVector3Make(hit.worldTransform.columns.3.x + node.geometry!.boundingBox.min.z, hit.worldTransform.columns.3.y, hit.worldTransform.columns.3.z)
+        node.position = position
     }
 }
